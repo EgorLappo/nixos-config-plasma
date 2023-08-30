3ca89ef75da1ce0ffaafe285d95a53c03e5fbf86
Egor Lappo <elappo@stanford.edu>
2023-08-30 14:06:58 -0700

    typos

--- configuration.nix
+++ configuration.nix
@@ -114,6 +114,11 @@
     '';
   };
 
+  fonts.fonts = with pkgs; [
+    nerdfonts.override {
+      fonts = ["JetBrainsMono" "Iosevka"]; 
+    }
+  ];
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
